6efd153f31b14d89328681a0a6b2a6c9e2da1916
CONOR GERARD WALSH <conor.a.walsh.2021@mumail.ie>
2022-04-11 10:41:31 +0100

    fixed lads page

--- context/GlobalState.js
+++ context/GlobalState.js
@@ -227,7 +227,7 @@ export const GlobalProvider = ({ children }) => {
         const getTotalToPar = (arr) => {
             let total = 0;
             arr.forEach(player => {
-                if (player.status == 'wd') {
+                if (player.status == 'wd' || player.status == 'cut') {
                     total = total + player.total_to_par + 5;
                 } else {
                     total += player.total_to_par
